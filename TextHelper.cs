656fcdfd754ce881682bc25722fb176ebe8c73d0
Corby <chichols@rcp.com>
2021-08-02 22:14:49 -0500

    I got rid of WordParser, and added a new method to TextHelper.

--- TextHelper.cs
+++ TextHelper.cs
@@ -386,6 +386,73 @@ namespace DataJuggler.UltimateHelper
 
             #endregion
 
+            #region ExportTextLines(List<TextLine> lines, string mustContain, string mustNotContain)
+            /// <summary>
+            /// This method returns the Text Lines, with Environment.NewLine appended after each line.
+            /// </summary>
+            /// <param name="lines">A collection of TextLines to rebuild into a file or block'</param>
+            /// <param name="mustContainText">If present, a line must have this to be added.</param>
+            /// <param name="mustNotContainText">If present, a line with will not be added</param>
+            public string ExportTextLines(List<TextLine> lines, string mustContainText = "", string mustNotContainText = "")
+            {
+                // initial value
+                string result = "";
+
+                // locals
+                bool mustContain = TextHelper.Exists(mustContainText);
+                bool mustNotContain = TextHelper.Exists(mustNotContainText);
+                bool addLine = false;
+
+                // If the lines collection exists and has one or more items
+                if (ListHelper.HasOneOrMoreItems(lines))
+                {
+                    // Create a new instance of a 'StringBuilder' object.
+                    StringBuilder sb = new StringBuilder();
+
+                    // Iterate the collection of TextLine objects
+                    foreach (TextLine line in lines)
+                    {
+                        // reset, default to true
+                        addLine = true;
+
+                        // if the value for mustContain is true
+                        if (mustContain)
+                        {
+                            // add line is true if this string contains the mustContainText
+                            addLine = line.Text.Contains(mustContainText);
+                        }
+
+                        // if addLine is true,a nd the mustNotContain is true
+                        if (addLine && mustNotContain)
+                        {
+                            // if the line contains the mustNotContainText                            
+                            if (line.Text.Contains(mustNotContainText))
+                            {
+                                // do not add this line
+                                addLine = false;
+                            }
+                        }
+
+                        // if addLine is true
+                        if (addLine)
+                        {
+                            // Add this line
+                            sb.Append(line.Text);
+
+                            // Add a NewLine char
+                            sb.Append(Environment.NewLine);
+                        }
+                    }
+
+                    // Set the result
+                    result = sb.ToString();
+                }
+                
+                // return value
+                return result;
+            }
+            #endregion
+            
             #region GetSpacesCount(string text)
             /// <summary>
             /// This method returns the number of preceding spaces in the text given.
