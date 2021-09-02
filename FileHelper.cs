ce742b13f1064110f9422caac76347ef6d99ee53
Corby <chichols@rcp.com>
2021-09-02 07:56:55 -0500

    I added a new method FileHelper.Exists.

--- FileHelper.cs
+++ FileHelper.cs
@@ -4,10 +4,8 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
+using System.Linq;
 
 #endregion
 
@@ -113,6 +111,31 @@ namespace DataJuggler.UltimateHelper
             }
             #endregion
             
+            #region Exists(string filePath)
+            /// <summary>
+            /// method returns the
+            /// </summary>
+            public static bool Exists(string filePath)
+            {
+                // initial value
+                bool exists = false;
+
+                // if the string exists
+                if (TextHelper.Exists(filePath))
+                {
+                    // if the fileExists
+                    if (System.IO.File.Exists(filePath))
+                    {
+                        // set to true
+                        exists = true;
+                    }
+                }
+
+                // return value
+                return exists;
+            }
+            #endregion
+
             #region GetFileNameWithoutExtension(string fullName)
             /// <summary>
             /// This method returns the File Name Without Extension
