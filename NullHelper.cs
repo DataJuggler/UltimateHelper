f063ed5f079f3d1d864bcd746e5d19f962b54941
DataJuggler <corby@datajuggler.com>
2022-05-03 20:38:03 -0500

    One more override to NullHelper.

--- NullHelper.cs
+++ NullHelper.cs
@@ -105,6 +105,20 @@ namespace DataJuggler.UltimateHelper
             }
             #endregion
 
+            #region Exists(object item, object item2, object item3, object item4, object item5, object item6, object item7)
+            /// <summary>
+            /// This method returns true if all seven objects exist.
+            /// </summary>
+            public static bool Exists(object item, object item2, object item3, object item4, object item5, object item6, object item7)
+            {
+                // initial value
+                bool exists = ((item != null) && (item2 != null) && (item3 != null) && (item4 != null) && (item5 != null) && (item6 != null) && (item7 != null));
+
+                // return value
+                return exists;
+            }
+            #endregion
+
             #region IsNull(object item)
             /// <summary>
             /// This method returns true if the item does not exist
@@ -183,6 +197,20 @@ namespace DataJuggler.UltimateHelper
             {
                 // initial value
                 bool isNull = ((item == null) || (item2 == null) || (item3 == null) || (item4 == null) || (item5 == null) || (item6 == null));
+
+                // return value
+                return isNull;
+            }
+            #endregion
+
+            #region IsNull(object item, object item2, object item3, object item4, object item5, object item6, object item7)
+            /// <summary>
+            /// This method returns true if any of the seven items Do Not Exist.
+            /// </summary>
+            public static bool IsNull(object item, object item2, object item3, object item4, object item5, object item6, object item7)
+            {
+                // initial value
+                bool isNull = ((item == null) || (item2 == null) || (item3 == null) || (item4 == null) || (item5 == null) || (item6 == null)  || (item7 == null));
 
                 // return value
                 return isNull;
