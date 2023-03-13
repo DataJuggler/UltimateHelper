0c62e81c356b774301e1dbf3fce13b4a67580dc4
DataJuggler <corby@datajuggler.com>
2023-03-13 09:32:03 -0500

    Version 7.1.2 has updates to EnvironmentVariableHelper.

--- EnvironmentVariableHelper.cs
+++ EnvironmentVariableHelper.cs
@@ -9,13 +9,13 @@ namespace DataJuggler.UltimateHelper
 
         #region Methods
 
-            #region GetEnvironmentVariableValue(string variableName)
+            #region GetEnvironmentVariableValue(string variableName, EnvironmentVariableTarget target)
             /// <summary>
             /// This method is used to get an environment value
             /// </summary>
             /// <param name="variableName"></param>
             /// <returns></returns>
-            public static string GetEnvironmentVariableValue(string variableName)
+            public static string GetEnvironmentVariableValue(string variableName, EnvironmentVariableTarget target)
             {
                 // initial value
                 string value = "";
@@ -25,7 +25,7 @@ namespace DataJuggler.UltimateHelper
                     if (Environment.OSVersion.Platform == PlatformID.Win32NT)
                     {
                         // Change the directory to %WINDIR%
-                        value = Environment.GetEnvironmentVariable(variableName, EnvironmentVariableTarget.Machine);
+                        value = Environment.GetEnvironmentVariable(variableName, target);
                     }
                 }
                 catch (Exception error)
@@ -36,6 +36,42 @@ namespace DataJuggler.UltimateHelper
 
                 // return value
                 return value;
+            }
+            #endregion
+
+            #region SetEnvironmentVariableValue(string name, string value, EnvironmentVariableTarget target)
+            /// <summary>
+            /// returns the Environment Variable Value
+            /// </summary>
+            public static bool SetEnvironmentVariableValue(string variableName, string value, EnvironmentVariableTarget target)
+            {
+                // initial value
+                bool valueSet = false;
+
+                try
+                {
+                    // if Windows
+                    if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+                    {
+                        // Create the EnvironmentVariable value
+                        Environment.SetEnvironmentVariable(variableName, value, target);
+
+                        // set the value
+                        valueSet = true;
+                    }
+                    else
+                    {
+                        // else I only use windows so others platforms are not supported.
+                    }
+                }
+                catch (Exception error)
+                {
+                    // for debugging only, do something else with it if you need to
+                    string err = error.ToString();
+                }
+                
+                // return value
+                return valueSet;
             }
             #endregion
 
